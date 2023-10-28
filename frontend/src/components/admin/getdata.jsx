aa2d194a52e5f184f0680e6f7a09a3ee57383f83
Raghav <90557042+Funik69@users.noreply.github.com>
2023-10-28 18:12:45 +0530

    resolve same class issue in contact and getdata page

--- frontend/src/components/admin/getdata.jsx
+++ frontend/src/components/admin/getdata.jsx
@@ -1,5 +1,6 @@
 import React ,{useEffect,useState} from 'react';
 import axios from 'axios';
+import './getdata.css';
 
  function Getdata(){
     const [data,setData] = useState([]);
@@ -13,11 +14,44 @@ import axios from 'axios';
         });
     },[]); 
   return (
-    <div>
+    <>
+    <div className='portCover'>
+    <h1>List of Registered Startups</h1>
+    </div>
+     <div className='showdata'>
       {data.map(item => (
-        <div key={item._id}>{item.location}</div>
+        <div className='cardData' key={item._id}>
+          <div className='card-item'><strong>Startup Name:</strong> {item.startupName}</div>
+          <div className='card-item'><strong>Founder Name:</strong> {item.founderName}</div>
+          <div className='card-item'><strong>Mobile Number:</strong> {item.mobileNumber}</div>
+          <div className='card-item'><strong>Alternate Number:</strong> {item.alternateNumber}</div>
+          <div className='card-item'><strong>Email:</strong> {item.email}</div>
+          <div className='card-item'><strong>Location:</strong> {item.location}</div>
+          <div className='card-item'><strong>State:</strong> {item.state}</div>
+          <div className='card-item'><strong>Pincode:</strong> {item.pinCode}</div>
+          <div className='card-item'><strong>Business Idea:</strong> {item.businessIdea}</div>
+          <div>
+            <a href={item.businessModelFile} target="_blank" rel="noopener noreferrer">
+              Link to Business Model
+            </a>
+          </div>
+          <div className='card-item'><strong>Why Join Us:</strong> {item.whyJoinUs}</div>
+          <div className='card-item'><strong>Registered:</strong> {item.registered}</div>
+          <div className='card-item'><strong>Development:</strong> {item.development}</div>
+          <div className='card-item'><strong>Successful:</strong> {item.successful}</div>
+            <a href={item.linkedinProfile} target="_blank" rel="noopener noreferrer">
+              LinkedIn
+            </a>
+            <hr></hr>
+            <div className='decisionButton'>
+            <button className='Abtn'>Accept</button>
+            <button className='Rbtn'>Reject</button>
+           </div> 
+        </div>
       ))}
     </div>
+    </>
+   
   )
 }
 
